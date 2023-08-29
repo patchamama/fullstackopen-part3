cf8865ed36c9c31fe1f2eec73ae7f0b265dd2b10
A. Urquiola <patchamama.go@gmail.com>
2023-08-30 01:50:01 +0200

    Add solution 3.16

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 /* eslint-disable comma-dangle */
 /* eslint-disable semi */
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const cors = require('cors');
+const Phonebook = require('./models/phonebook');
 
 // midlewares
-// Same origin policy
-app.use(cors());
+// Serving static files from the backend
+app.use(express.static('build'));
 // To parse the body of the request
 app.use(express.json());
+// Same origin policy
+app.use(cors());
 // To log messages to the console
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
 );
-// Serving static files from the backend
-app.use(express.static('build'));
 
 let phonebooks = [
   {
@@ -46,8 +48,14 @@ app.get('/', (request, response) => {
 });
 
 app.get('/api/persons', (request, response) => {
-  response.json(phonebooks);
+  Phonebook.find({}).then((phonebooks) => {
+    response.json(phonebooks);
+  });
 });
+
+// app.get('/api/persons', (request, response) => {
+//   response.json(phonebooks);
+// });
 
 app.get('/api/info', (request, response) => {
   response.send(
@@ -59,27 +67,62 @@ app.get('/api/info', (request, response) => {
 });
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const phonebook = phonebooks.find((phonebook) => phonebook.id === id);
-
-  if (phonebook) {
-    response.json(phonebook);
-  } else {
-    response.status(404).end();
-  }
+  Phonebook.findById(request.params.id)
+    .then((phonebook) => {
+      if (phonebook) {
+        response.json(phonebook);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  phonebooks = phonebooks.filter((phonebook) => phonebook.id !== id);
+// app.get('/api/persons/:id', (request, response) => {
+//   const id = Number(request.params.id);
+//   const phonebook = phonebooks.find((phonebook) => phonebook.id === id);
 
-  response.status(204).end();
+//   if (phonebook) {
+//     response.json(phonebook);
+//   } else {
+//     response.status(404).end();
+//   }
+// });
+
+// app.delete('/api/persons/:id', (request, response) => {
+//   const id = Number(request.params.id);
+//   phonebooks = phonebooks.filter((phonebook) => phonebook.id !== id);
+
+//   response.status(204).end();
+// });
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Phonebook.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
-const getRandomId = () => {
-  const min = phonebooks.length;
-  return Math.random() * (100000 - min) + min;
-};
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body;
+
+  const phonebook = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Phonebook.findByIdAndUpdate(request.params.id, phonebook, { new: true })
+    .then((updatedPhonebook) => {
+      response.json(updatedPhonebook);
+    })
+    .catch((error) => next(error));
+});
+
+// const getRandomId = () => {
+//   const min = phonebooks.length;
+//   return Math.random() * (100000 - min) + min;
+// };
 
 app.post('/api/persons', (request, response) => {
   const body = request.body;
@@ -90,27 +133,62 @@ app.post('/api/persons', (request, response) => {
     });
   }
 
-  const nameExist = phonebooks.find(
-    (phonebook) => phonebook.name === body.name
-  );
-  if (nameExist) {
-    return response.status(400).json({
-      error: 'name must be unique',
-    });
-  }
-
-  const phonebook = {
+  const phonebook = new Phonebook({
     name: body.name,
     number: body.number,
-    id: getRandomId(),
-  };
+  });
 
-  phonebooks = phonebooks.concat(phonebook);
-
-  response.json(phonebook);
+  phonebook.save().then((savedPhonebook) => {
+    response.json(savedPhonebook);
+  });
 });
 
-const PORT = process.env.PORT || 3001;
+// app.post('/api/persons', (request, response) => {
+//   const body = request.body;
+
+//   if (!body.name || !body.number) {
+//     return response.status(400).json({
+//       error: 'The name or number is missing',
+//     });
+//   }
+
+//   const nameExist = phonebooks.find(
+//     (phonebook) => phonebook.name === body.name
+//   );
+//   if (nameExist) {
+//     return response.status(400).json({
+//       error: 'name must be unique',
+//     });
+//   }
+
+//   const phonebook = {
+//     name: body.name,
+//     number: body.number,
+//     id: getRandomId(),
+//   };
+
+//   phonebooks = phonebooks.concat(phonebook);
+
+//   response.json(phonebook);
+// });
+
+// Invalid routes
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+app.use(unknownEndpoint);
+// Error handling in middleware
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+  next(error);
+};
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
